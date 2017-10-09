704d2b4b66d449e2bf85bf0141a95d8431322df4
kristianparvanov <kristian.parvanov95@gmail.com>
2017-10-10 00:17:37 +0300

    added some implentations in Account and Category

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,5 +1,19 @@
 package model;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
 
+	private String name;
+	private BigDecimal amount;
+	private List<Transaction> transactions;
+	
+	public Account(String name, BigDecimal amount) {
+		this.name = name;
+		this.amount = amount;
+		
+		transactions = new ArrayList<>();
+	}
 }
--- src/model/Category.java
+++ src/model/Category.java
@@ -2,4 +2,13 @@ package model;
 
 public class Category {
 
+	private String name;
+	private String type;
+	
+	public Category(String name, String type) {
+		this.name = name;
+		this.type = type;
+	}
+	
+	
 }
