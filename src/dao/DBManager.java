062d7471da553059a08c221860633858413e2ced
unknown <nikolovblagoy@gmail.com>
2017-10-10 01:23:39 +0300

    added DBManager, transactionDAO

---
+++ src/dao/DBManager.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBManager {
+    private static DBManager instance;
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/finance_tracker";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root"; 
+    private Connection conn;
+
+    private DBManager() {
+        try {
+            Class.forName(DBManager.JDBC_DRIVER);
+            this.conn = DriverManager.getConnection(DBManager.DB_URL, DBManager.USERNAME, DBManager.PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Unable to load database driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Unable to connect to database: " + e.getMessage());
+        }
+    }
+
+    public static DBManager getInstance() {
+        if (DBManager.instance == null) {
+            synchronized (DBManager.class) {
+                if (DBManager.instance == null) {
+                    DBManager.instance = new DBManager();
+                }
+            }
+        }
+        return DBManager.instance;
+    }
+
+    public Connection getConnection() {
+        return this.conn;	
+    }
+
+    public void closeConnection() {
+        try {
+            this.conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/dao/TransactionDAO.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.math.BigDecimal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Transaction;
+
+public class TransactionDAO {
+	private static TransactionDAO instance;
+	
+	public synchronized static TransactionDAO getInstance() {
+		if (instance == null) {
+			instance = new TransactionDAO();
+		}
+		return instance;
+	}
+	
+	public List<Transaction> getAllTransactions() throws SQLException {
+		List<Transaction> transactions = new ArrayList<Transaction>();
+		String query = "SELECT transaction_id, type, date, amount, account, category, own_category FROM finance_tracker.transactions";
+		
+		PreparedStatement statement = null;
+		statement = DBManager.getInstance().getConnection().prepareStatement(query);
+		ResultSet result = statement.executeQuery();
+		while (result.next()) {
+			int transactionId = result.getInt("transaction_id");
+			String type = result.getString("type");
+			LocalDateTime date = result.getTimestamp("date").toLocalDateTime();
+			BigDecimal amount = result.getBigDecimal("amount");
+			String account = result.getString("account");
+			String category = result.getString("category");
+			String ownCategory = result.getString("own_category");
+			//Transaction t = new Transaction(transactionId, type, amount, account, category, ownCategory, date, tags);
+			//transactions.add(t);
+		}
+		return transactions;
+	}
+}
